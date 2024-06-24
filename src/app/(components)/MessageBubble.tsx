67e050c16fb4989cca9d874d67cce23895ba7201
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-24 19:17:36 +0100

    changed text

--- src/app/(components)/MessageBubble.tsx
+++ src/app/(components)/MessageBubble.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const MessageBubble = () => {
   return (
-    <section className="w-1/4 h-16 flex justify-center items-center absolute left-40 -top-4 rounded-tl-3xl rounded-br-3xl rounded-tr-3xl border">
-      <p>Hello! :)</p>
+    <section className="w-fit h-16 flex justify-center items-center absolute left-40 sm:-top-4 px-2 rounded-tl-3xl rounded-br-3xl rounded-tr-3xl border">
+      <p>
+        Hello! :) My name is <strong>Vanja.</strong>
+      </p>
     </section>
   );
 };
