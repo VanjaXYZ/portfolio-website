40cac7f5deb3955bc5a21f33850c0ea0d7dfe70c
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-24 17:47:16 +0100

    created

---
+++ src/app/(components)/MessageBubble.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MessageBubble = () => {
+  return (
+    <section className="w-1/4 h-16 flex justify-center items-center absolute left-40 -top-4 rounded-tl-3xl rounded-br-3xl rounded-tr-3xl border">
+      <p>Hello! :)</p>
+    </section>
+  );
+};
+
+export default MessageBubble;
