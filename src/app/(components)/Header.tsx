2f04f68c4d9eadadaef6d2c9810b3c7f7a550c52
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-24 19:17:22 +0100

    add mobile header

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -1,10 +1,28 @@
+import { FolderKanban, House, Info } from "lucide-react";
 import HeaderLinks from "./HeaderLinks";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex justify-between px-12 py-2 sticky top-0 bg-black/90">
+    <header className="justify-between px-12 py-2 sticky top-0 bg-black/90 sm:flex hidden z-10">
       <p>Vanja Šukurma</p>
       <HeaderLinks />
+    </header>
+  );
+};
+
+export const MobileHeader = () => {
+  return (
+    <header className="flex sm:hidden sticky bottom-0 justify-between px-12 py-2 bg-black/90 w-full">
+      <Link href={"#about"} className="font-bold tracking-widest">
+        <House />
+      </Link>
+      <Link href={"#work"} className="font-bold tracking-widest">
+        <FolderKanban />
+      </Link>
+      <Link href={"#contact"} className="font-bold tracking-widest">
+        <Info />
+      </Link>
     </header>
   );
 };
