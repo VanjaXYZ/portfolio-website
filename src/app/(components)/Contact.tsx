7ee05e5bd4e6a478558463bc452d666142b7a6f8
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-24 17:47:13 +0100

    add social network links

--- src/app/(components)/Contact.tsx
+++ src/app/(components)/Contact.tsx
@@ -1,85 +1,22 @@
 import React from "react";
+import GithubIcon from "../assets/github.png";
+import LinkedinIcon from "../assets/linkedin.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const Contact = () => {
   return (
-    <section id="contact" className="flex flex-col space-y-6 min-h-screen">
-      <h2 className="text-2xl font-bold">Contact</h2>
-      <article>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima veniam
-          dignissimos, accusantium explicabo, optio laudantium necessitatibus,
-          alias assumenda voluptate illo minus a dolorum dolores. A magni
-          nesciunt fugit consequuntur, deleniti repudiandae recusandae vitae,
-          pariatur, labore voluptatum harum sint impedit quia nulla sapiente!
-          Quisquam iusto in sit laborum dolor, officiis voluptatibus ipsa
-          debitis explicabo odio, libero similique magni minima beatae aliquam
-          quidem? Autem, inventore reiciendis temporibus odio fuga illum, hic
-          quod praesentium laborum quisquam minima, quidem itaque! Mollitia
-          beatae necessitatibus pariatur rerum quaerat, repellendus blanditiis
-          molestiae deleniti? Autem voluptatem quae debitis. Laboriosam totam
-          ipsam qui placeat aperiam et atque fugit delectus nemo optio, enim
-          dignissimos consequatur ea nobis molestias reiciendis, cupiditate
-          beatae voluptates. Cum omnis at id explicabo necessitatibus ab
-          aspernatur delectus voluptates facere? Nobis maiores veritatis vitae?
-          Deleniti dicta, iste repellat culpa unde sequi nobis dolorem minima!
-          Aliquid delectus ipsum at recusandae numquam molestias eius cumque
-          quia, eaque blanditiis modi voluptate perspiciatis, cupiditate fugit
-          labore incidunt tempore ratione dolores quibusdam quis. Non architecto
-          necessitatibus atque laudantium est libero, alias a quia cumque
-          perferendis eligendi, consequuntur, dolores hic. Cumque dolorem quas
-          odit, debitis amet exercitationem repellendus quasi. Quas tempora
-          aperiam enim id nesciunt, minima natus laborum fugit, adipisci
-          molestias amet error.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-          veritatis veniam culpa quibusdam, harum sunt at doloribus voluptate
-          impedit facilis unde officia. Possimus est omnis amet ab explicabo,
-          velit esse aliquid quis? Porro repudiandae, magni aliquid
-          necessitatibus facilis earum fugit dolore modi praesentium consectetur
-          sed unde quo ipsa! Reiciendis fugit libero voluptatum ullam
-          perferendis nesciunt enim earum magnam nostrum, tenetur maxime nobis
-          accusantium sed dicta officia suscipit, consequuntur laudantium non.
-          Sit nam obcaecati ex voluptates, ea tempore totam suscipit. Quisquam
-          quidem mollitia odio, nobis voluptate molestias aperiam maiores
-          accusantium assumenda totam eum, dignissimos quos adipisci velit
-          ducimus et, expedita reprehenderit deserunt sint commodi dolores illum
-          quae esse. Tempora maiores eveniet ratione optio expedita tenetur
-          officiis eaque eos dolorem assumenda. Eius illum, non voluptatem
-          necessitatibus soluta tempore libero iste hic reprehenderit
-          praesentium possimus magnam vero excepturi. Fugit, obcaecati quo!
-          Deserunt perspiciatis sit sequi nihil rerum earum praesentium animi
-          repellat! Cumque aut molestiae tempore a et. Cumque, quia inventore.
-          Eos consectetur, quidem nemo, aut sunt possimus dolorum atque maiores
-          delectus totam voluptates qui? Ex iure numquam sed in, aperiam sunt
-          ipsam a eum aspernatur voluptate, corrupti vitae autem consectetur
-          laboriosam expedita explicabo mollitia eius doloremque ducimus cumque
-          error, animi incidunt labore. Adipisci repellat est placeat libero at,
-          minima, quidem tenetur corporis pariatur facilis magni labore
-          architecto nulla expedita sint sed, officia neque maiores? Quisquam
-          tempore ut facere ipsum eos, consectetur sequi velit modi ad
-          repellendus iusto natus quas? Minima delectus similique voluptatum,
-          quibusdam non cumque velit perferendis doloribus nesciunt eligendi ad
-          nulla tempora at? Libero voluptates iure similique deleniti quis culpa
-          eos reprehenderit sed incidunt reiciendis! Soluta repellat facilis in,
-          perferendis ullam voluptatem nemo velit quis. Quas tempora delectus
-          rerum consectetur fuga qui nesciunt quibusdam vero quo aliquid. Facere
-          placeat cupiditate magni fugiat voluptate! Mollitia qui eligendi
-          accusamus facere nemo deserunt, perferendis ab dolores culpa sed quos
-          itaque aperiam architecto officiis officia, quidem ex, inventore sunt
-          quod consectetur tempora ad esse sit. Quos cum deserunt beatae
-          architecto dolores soluta blanditiis necessitatibus possimus accusamus
-          similique autem, natus quas delectus eum et sequi, vero explicabo quo!
-          In quia repudiandae quasi voluptatibus repellendus nihil placeat quam
-          aperiam illo minima beatae natus quis eveniet voluptatem voluptate
-          unde perferendis ipsum, suscipit culpa tempore! Sequi quod maxime
-          ratione exercitationem libero quibusdam labore modi vitae accusamus
-          dignissimos sapiente est sed consequuntur dolores culpa, quis
-          distinctio adipisci ab at a minima illum ipsum praesentium! At
-          possimus ipsam nobis architecto eligendi?
-        </p>
-      </article>
-    </section>
+    <footer id="contact" className="flex">
+      <Link href={"https://github.com/VanjaXYZ"} target="_blank">
+        <Image src={GithubIcon} width={150} height={150} alt="Github" />
+      </Link>
+      <Link
+        href={"https://www.linkedin.com/in/vanja-sukurma-458905250/"}
+        target="_blank"
+      >
+        <Image src={LinkedinIcon} width={150} height={150} alt="LinkedIn" />
+      </Link>
+    </footer>
   );
 };
 
