9c3eb74e0e66a7730f25daee48ad4349e6cc192f
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-24 18:49:05 +0100

    create mobile version image

--- src/app/(components)/AvatarImage.tsx
+++ src/app/(components)/AvatarImage.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Image from "next/image";
-import React from "react";
-import AvatarImg from "../assets/avatar_no_background.png";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import AvatarImg from "../assets/avatar_no_background.png";
+import MessageBubble from "./MessageBubble";
 
 const AvatarImage = () => {
   return (
@@ -24,6 +24,30 @@ const AvatarImage = () => {
         className="rounded-full overflow-hidden drop-shadow-[0_35px_35px_rgba(255,255,255,0.1)]"
       />
     </motion.section>
+  );
+};
+
+export const AvatarImageMobile = () => {
+  return (
+    <motion.picture
+      className="flex lg:hidden relative"
+      initial={{ opacity: 0, scale: 0.1 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.3,
+        delay: 0.1,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    >
+      <Image
+        src={AvatarImg}
+        width={650}
+        height={800}
+        alt="Avatar image"
+        className="rounded-full overflow-hidden max-w-52"
+      />
+      <MessageBubble />
+    </motion.picture>
   );
 };
 
