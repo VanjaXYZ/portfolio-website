b14409a0399c3e8a7035faf0195629e4ddd54c13
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-24 19:16:58 +0100

    changed intro text

--- src/app/(components)/About.tsx
+++ src/app/(components)/About.tsx
@@ -4,22 +4,18 @@ const About = () => {
   return (
     <section
       id="about"
-      className="flex flex-col space-y-6 min-h-screen lg:max-w-md self-start"
+      className="flex flex-col space-y-6 sm:min-h-screen lg:max-w-md self-start"
     >
       <AvatarImageMobile />
       <h2 className="text-2xl font-bold tracking-wider">About me</h2>
       <article>
         <p className="xl:text-xl text-sm tracking-wider lg:text-lg">
-          I am passionate about <strong>teamwork and communication</strong>, and
-          I believe these skills are essential for successful project
-          collaboration.
-        </p>
-        <p className="xl:text-xl text-sm tracking-wider lg:text-lg">
-          Additionally, I am an enthusiastic learner, always seeking to improve
-          and advance my technical and professional abilities. Outside of work,
-          I enjoy playing the guitar, socializing, and caring for animals. I am
-          eager to bring my dedication and enthusiasm to your team and
-          contribute to the success of your projects.
+          Hello! My name is <strong>Vanja Sukurma</strong>, a passionate
+          frontend developer with over 2 years of practical experience. I
+          specialize in React and Next.js environments, focusing on creating
+          intuitive and user-friendly web applications. In my work, I emphasize
+          efficiency, scalability, and aesthetics, and I enjoy the challenges
+          that come with the dynamic world of web technologies.
         </p>
       </article>
     </section>
