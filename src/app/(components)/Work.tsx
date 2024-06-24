48e5d6d37bdc151098a1f79ce5e902a18b320918
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-24 19:17:43 +0100

    removed dummy text

--- src/app/(components)/Work.tsx
+++ src/app/(components)/Work.tsx
@@ -2,60 +2,9 @@ import React from "react";
 
 const Work = () => {
   return (
-    <section id="work" className="flex flex-col space-y-6">
-      <h2 className="text-2xl font-bold">Work</h2>
-      <article>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui in nam
-          accusamus nobis aliquid obcaecati saepe laborum provident neque
-          ducimus commodi cum velit dolores officia, ut, numquam cumque fugiat
-          enim reiciendis aliquam voluptatibus. Perferendis eveniet doloribus
-          odit velit quisquam illum atque nemo delectus cum. Eius enim cum quod
-          iste repellat! Beatae mollitia nulla officiis aliquam quisquam laborum
-          assumenda asperiores! Ipsa, error laboriosam doloribus alias
-          cupiditate eius necessitatibus eos temporibus id unde adipisci officia
-          quos expedita qui repellendus aliquam dolorum. Asperiores laboriosam
-          praesentium, ducimus odit recusandae accusantium doloribus sequi
-          nesciunt laudantium aliquam ea dolore soluta consectetur a, expedita
-          eum temporibus voluptas?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nihil
-          tenetur totam architecto vero culpa commodi delectus vel? Soluta
-          aperiam et odit deserunt vel vitae illo rerum eius est perferendis,
-          ratione animi doloribus suscipit exercitationem modi dolorem nisi
-          magni tenetur cupiditate ad provident nulla aliquid. Perferendis, nisi
-          corporis? Non corporis voluptatum nostrum corrupti accusantium
-          incidunt, impedit, inventore eius nulla vitae, illum recusandae
-          quaerat architecto mollitia veritatis repudiandae eaque qui fugit
-          assumenda. Minus, fugit deleniti quasi et doloribus earum laboriosam
-          mollitia alias molestias esse consequuntur fugiat? Non, distinctio
-          voluptates impedit aspernatur eaque aperiam vero! Unde consequuntur
-          necessitatibus at, recusandae nam minus voluptates earum facere
-          commodi dicta facilis tenetur id debitis numquam, soluta animi
-          voluptatem maiores repellendus suscipit quibusdam, sit assumenda.
-          Nihil, nemo! Harum quas veniam, expedita nobis praesentium nesciunt
-          velit qui sequi magnam id aut aperiam maxime voluptatem commodi nulla,
-          consequatur maiores. Quaerat modi, odit ex quo harum laboriosam qui ut
-          provident delectus distinctio quam, libero minima tempore excepturi
-          aperiam voluptatum iusto nemo magnam explicabo laudantium enim.
-          Repellendus corrupti recusandae cupiditate suscipit molestias, ratione
-          exercitationem porro aut iste numquam vero, eos sint sapiente ab non
-          eum quod odit excepturi praesentium. Repellendus harum nisi, magni
-          unde esse impedit nulla eligendi officia dolores id, dicta omnis
-          dolor? Fugiat autem aliquid, dolores ipsa dolorum, quo ex ducimus iure
-          voluptatibus ratione ullam? Possimus expedita, repellendus consectetur
-          sit accusamus amet! Nostrum eaque magnam quam obcaecati vitae expedita
-          necessitatibus molestias, officiis eum recusandae similique quis minus
-          ullam culpa cum ipsa repellendus totam quidem et corrupti sint
-          mollitia? Fugiat ipsum qui minima veniam animi accusamus rerum quas
-          neque inventore nobis eius, culpa nihil assumenda quam ea natus quod
-          quis odit itaque voluptatibus ducimus necessitatibus. Amet nesciunt
-          animi debitis reiciendis illo nostrum quis ipsam sequi atque provident
-          et quas tempora at quod odio deserunt, deleniti ipsum explicabo
-          corrupti? Dolores ea quia deserunt sit?
-        </p>
-      </article>
+    <section id="work" className="flex flex-col w-full space-y-6">
+      <h2 className="text-2xl font-bold text-start">Projects</h2>
+      <article></article>
     </section>
   );
 };
