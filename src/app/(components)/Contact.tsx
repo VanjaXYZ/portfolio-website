87cbdca5ba2f1cca971e194a4b924495af34b9de
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-26 00:28:14 +0100

    add icons instead images

--- src/app/(components)/Contact.tsx
+++ src/app/(components)/Contact.tsx
@@ -1,20 +1,17 @@
-import React from "react";
-import GithubIcon from "../assets/github.png";
-import LinkedinIcon from "../assets/linkedin.png";
-import Image from "next/image";
+import { Github, Linkedin } from "lucide-react";
 import Link from "next/link";
 
 const Contact = () => {
   return (
-    <footer id="contact" className="flex">
+    <footer id="contact" className="flex space-x-12">
       <Link href={"https://github.com/VanjaXYZ"} target="_blank">
-        <Image src={GithubIcon} width={80} height={80} alt="Github" />
+        <Github />
       </Link>
       <Link
         href={"https://www.linkedin.com/in/vanja-sukurma-458905250/"}
         target="_blank"
       >
-        <Image src={LinkedinIcon} width={80} height={80} alt="LinkedIn" />
+        <Linkedin />
       </Link>
     </footer>
   );
