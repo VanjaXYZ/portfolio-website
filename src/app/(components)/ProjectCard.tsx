dab9d60f7f9cf8888f710ea9d4061c0aa1ff0ace
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-26 22:36:37 +0100

    changed style of wrapper

--- src/app/(components)/ProjectCard.tsx
+++ src/app/(components)/ProjectCard.tsx
@@ -6,25 +6,25 @@ import clsx from "clsx";
 
 const ProjectCard = ({ project }: { project: Projects }) => {
   return (
-    <div className="py-2 border rounded-md px-1 w-full hover:bg-white hover:text-black flex flex-col group cursor-pointer gap-2">
+    <div className="py-2 border rounded-md px-1 w-full max-w-md flex flex-col group cursor-pointer gap-2">
       <h5 className="text-lg underline">{project.title}</h5>
       <em>{project.short_description}</em>
       <ul className="flex flex-wrap gap-2 my-2">
         {project.tech.map((tech: string) => (
           <li
             key={tech}
-            className="bg-white/5 rounded-md px-1 group-hover:bg-slate-200"
+            className="bg-white/5 rounded-md px-1 group-hover:bg-slate-500"
           >
             {tech}
           </li>
         ))}
       </ul>
-      <span className="bg-white/5 rounded-md px-1 w-fit group-hover:bg-slate-200">
+      <span className="bg-white/5 rounded-md px-1 w-fit group-hover:bg-slate-500">
         {project.type}
       </span>
       <span
         className={clsx(
-          "w-fit rounded-md px-1 group-hover:bg-slate-200 self-end",
+          "w-fit rounded-md px-1 group-hover:bg-slate-500 self-end",
           {
             "text-yellow-300": project.status === "In progress",
           },
