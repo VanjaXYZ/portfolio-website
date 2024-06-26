bb86931b824ba7b57ef377fa1c56127dd7d2ddc5
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-26 22:36:25 +0100

    add icons and classes

--- src/app/(components)/Contact.tsx
+++ src/app/(components)/Contact.tsx
@@ -4,12 +4,17 @@ import Link from "next/link";
 const Contact = () => {
   return (
     <footer id="contact" className="flex space-x-12">
-      <Link href={"https://github.com/VanjaXYZ"} target="_blank">
+      <Link
+        href={"https://github.com/VanjaXYZ"}
+        target="_blank"
+        className="bg-white/5 p-2 rounded-md"
+      >
         <Github />
       </Link>
       <Link
         href={"https://www.linkedin.com/in/vanja-sukurma-458905250/"}
         target="_blank"
+        className="bg-white/5 p-2 rounded-md"
       >
         <Linkedin />
       </Link>
