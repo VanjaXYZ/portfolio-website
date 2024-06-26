2076ab0caa663d0bd9c6b02cc9573122e25d3603
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-26 23:12:47 +0100

    add better styling

--- src/app/(components)/DownloadCVButton.tsx
+++ src/app/(components)/DownloadCVButton.tsx
@@ -3,7 +3,7 @@ const DownloadCVButton = () => {
     <a
       href={"/cv.pdf"}
       download={"vanja_sukurma_cv.pdf"}
-      className="bg-white/5 rounded-md p-2"
+      className="bg-white/5 rounded-md p-4 uppercase tracking-widest"
     >
       Download CV
     </a>
