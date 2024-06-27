0548efb0082c9f36385bdbd7d653e09dce14fa70
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-28 00:18:10 +0100

    add animations

--- src/app/(components)/ProjectCard.tsx
+++ src/app/(components)/ProjectCard.tsx
@@ -1,44 +1,51 @@
+"use client";
 import React from "react";
 import { Projects } from "../_lib/types";
 import clsx from "clsx";
 import Link from "next/link";
-
-// postavi kao linkove
+import { motion } from "framer-motion";
 
 const ProjectCard = ({ project }: { project: Projects }) => {
   return (
-    <Link
-      href={`/projects/${project?.id}`}
-      className="py-2 border rounded-md px-1 w-full max-w-md flex flex-col group cursor-pointer gap-2 h-64 hover:bg-white/5 overflow-y-auto"
+    <motion.div
+      initial={{ rotate: "0deg", opacity: 0 }}
+      whileHover={{ scale: 1.05, rotate: "2.5deg" }}
+      whileTap={{ scale: 0.93 }}
+      whileInView={{ opacity: 1, transition: { duration: 2 } }}
+      className="py-2 border rounded-md px-1 w-full max-w-md flex flex-col group cursor-pointer gap-2 hover:bg-white/5 overflow-y-auto"
     >
-      <h5 className="text-lg underline">{project.title}</h5>
-      <em>{project.short_description}</em>
-      <ul className="flex flex-wrap gap-2 my-2">
-        {project.tech.map((tech: string) => (
-          <li key={tech} className="bg-white/5 rounded-md px-1 ">
-            {tech}
-          </li>
-        ))}
-      </ul>
-      <div className="flex-grow">
-        <span className="bg-white/5 rounded-md p-1 w-fit ">{project.type}</span>
-      </div>
-      <span
-        className={clsx(
-          "w-fit rounded-md px-1  self-end",
-          {
-            "border border-yellow-400 uppercase font-semibold":
-              project.status === "In progress",
-          },
-          {
-            "border-green-400 border font-semibold uppercase":
-              project.status === "Completed",
-          }
-        )}
-      >
-        {project.status}
-      </span>
-    </Link>
+      <Link href={`/projects/${project?.id}`} className="flex flex-col h-64">
+        <h5 className="text-lg underline">{project.title}</h5>
+        <em>{project.short_description}</em>
+        <ul className="flex flex-wrap gap-2 my-2">
+          {project.tech.map((tech: string) => (
+            <li key={tech} className="bg-white/5 rounded-md px-1 ">
+              {tech}
+            </li>
+          ))}
+        </ul>
+        <div className="flex-grow">
+          <span className="bg-white/5 rounded-md p-1 w-fit ">
+            {project.type}
+          </span>
+        </div>
+        <span
+          className={clsx(
+            "w-fit rounded-md px-1  self-end",
+            {
+              "border border-yellow-400 uppercase font-semibold":
+                project.status === "In progress",
+            },
+            {
+              "border-green-400 border font-semibold uppercase":
+                project.status === "Completed",
+            }
+          )}
+        >
+          {project.status}
+        </span>
+      </Link>
+    </motion.div>
   );
 };
 
