8ccc9baf747d4753b55ecf8c3125808905de12ba
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-27 16:46:18 +0100

    onHover enlight button

--- src/app/(components)/DownloadCVButton.tsx
+++ src/app/(components)/DownloadCVButton.tsx
@@ -3,7 +3,7 @@ const DownloadCVButton = () => {
     <a
       href={"/vanja_sukurma_cv.pdf"}
       download={"vanja_sukurma_cv.pdf"}
-      className="bg-white/5 rounded-md p-4 uppercase tracking-widest"
+      className="bg-white/5 rounded-md p-4 uppercase tracking-widest hover:bg-white/10"
     >
       Download CV
     </a>
