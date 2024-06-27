d1c4bc0e7b6bbb0b08bac1e16c2c9fe93f59b9f8
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-28 00:42:40 +0100

    wrapped into scroll animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,22 @@ import Contact from "./(components)/Contact";
 import DownloadCVButton from "./(components)/DownloadCVButton";
 import GreetingMessage from "./(components)/GreetingMessage";
 import { MobileHeader } from "./(components)/Header";
+import ScrollWrapper from "./(components)/ScrollWrapper";
 import Work from "./(components)/Work";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2 sm:p-24 space-y-10 relative">
-      {/* <GreetingMessage /> */}
-      <section className="flex w-full justify-center">
-        <About />
-        <AvatarImage />
-      </section>
-      <Work />
-      <DownloadCVButton />
-      <Contact />
-    </main>
+    <ScrollWrapper>
+      <main className="flex min-h-screen flex-col items-center justify-between p-2 sm:p-24 space-y-10 relative">
+        {/* <GreetingMessage /> */}
+        <section className="flex w-full justify-center">
+          <About />
+          <AvatarImage />
+        </section>
+        <Work />
+        <DownloadCVButton />
+        <Contact />
+      </main>
+    </ScrollWrapper>
   );
 }
