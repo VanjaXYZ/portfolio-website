2760f43c2d2feacff0776b2d1716db80c8b12210
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-27 16:41:06 +0100

    fix download file name

--- src/app/(components)/DownloadCVButton.tsx
+++ src/app/(components)/DownloadCVButton.tsx
@@ -1,7 +1,7 @@
 const DownloadCVButton = () => {
   return (
     <a
-      href={"/cv.pdf"}
+      href={"/vanja_sukurma_cv.pdf"}
       download={"vanja_sukurma_cv.pdf"}
       className="bg-white/5 rounded-md p-4 uppercase tracking-widest"
     >
