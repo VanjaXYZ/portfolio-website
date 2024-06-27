41fb088685ab8e2d5a1c3f95dbef94279ab91730
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-27 16:18:50 +0100

    add full description

--- src/app/_lib/projects-data.ts
+++ src/app/_lib/projects-data.ts
@@ -4,7 +4,9 @@ export const projects = [
     title: "Online Book Library",
     type: "Fullstack",
     short_description: "Create your own online book library!",
-    tech: ["Next.js 14", "Nest.js", "TailwindCSS"],
+    complete_description:
+      "Online Book Library je sveobuhvatna aplikacija koja omogućava korisnicima da kreiraju i upravljaju svojim digitalnim bibliotekama. Korisnici mogu dodavati knjige, uređivati informacije o knjigama, pratiti svoj napredak u čitanju i deliti preporuke sa prijateljima. Aplikacija koristi Next.js 14 za brzu i efikasnu izradu frontend dela, dok Nest.js obezbeđuje robustnu i skalabilnu backend arhitekturu. Tailwind CSS je korišćen za kreiranje responzivnog i atraktivnog korisničkog interfejsa. Ovaj projekat predstavlja kompletno rešenje za sve ljubitelje knjiga koji žele da organizuju i uživaju u svojoj kolekciji na mreži.",
+    tech: ["Next.js 14", "Nest.js", "Tailwind CSS"],
     status: "Completed",
   },
   {
@@ -12,7 +14,9 @@ export const projects = [
     title: "My AI Diary",
     type: "Fullstack",
     short_description: "Write your daily diary entries with AI helper...",
-    tech: ["Next.js 14", "Nest.js", "TailwindCSS", "shadCn"],
+    complete_description:
+      "My AI Diary je inovativna aplikacija koja koristi veštačku inteligenciju kako bi pomogla korisnicima da pišu i organizuju svoje dnevničke zapise. Ova aplikacija omogućava korisnicima da lako beleže svoje misli, osećanja i događaje iz svakodnevnog života, uz pomoć AI asistenta koji pruža sugestije, analizira emocije i predlaže poboljšanja. Sa Next.js 14, frontend je brz i responzivan, dok Nest.js osigurava snažnu backend podršku. shadCn i Tailwind CSS su korišćeni za moderni i privlačni dizajn korisničkog interfejsa. Ovaj projekat je idealan za sve koji žele da unaprede svoje pisanje i bolje razumeju sebe kroz interaktivne i inteligentne sugestije.",
+    tech: ["Next.js 14", "Nest.js", "shadCn", "Tailwind CSS"],
     status: "In progress",
   },
   {
@@ -20,7 +24,9 @@ export const projects = [
     title: "Football Stats",
     type: "Frontend",
     short_description: "Check the newest results in world of soccer...",
-    tech: ["Next.js 14", "TailwindCSS", "shadCn"],
+    complete_description:
+      "Football Stats je moderna aplikacija dizajnirana za sve ljubitelje fudbala koji žele da prate najnovije rezultate, statistike i vesti iz sveta fudbala. Aplikacija pruža korisnicima ažurirane informacije o utakmicama, timovima i igračima, omogućavajući im da ostanu informisani o svim važnim dešavanjima. Koristeći Next.js 14 za izradu frontend dela, aplikacija nudi brzo i efikasno korisničko iskustvo, dok Tailwind CSS i shadCn doprinose stvaranju modernog i privlačnog dizajna. Ovaj projekat je u toku i predstavlja izvrsno rešenje za sve koji žele da budu u toku sa svetskim fudbalskim događajima.",
+    tech: ["Next.js 14", "Tailwind CSS", "shadCn"],
     status: "In progress",
   },
   {
@@ -28,7 +34,9 @@ export const projects = [
     title: "Beehive",
     type: "Frontend",
     short_description: "Learn all about beekeeping in one place..",
-    tech: ["Next.js 14", "TailwindCSS", "shadCn"],
+    complete_description:
+      "Beehive je sveobuhvatna aplikacija namenjena svima koji žele da nauče više o pčelarstvu. Ova aplikacija pruža korisnicima pristup bogatom sadržaju o pčelama, tehnikama pčelarstva, opremi, kao i savetima za početnike i iskusne pčelare. Sa Next.js 14, frontend aplikacije je brz i responzivan, dok Tailwind CSS i shadCn omogućavaju kreiranje atraktivnog i modernog dizajna. Beehive je savršeno mesto za sve koji žele da istraže svet pčelarstva, bilo da su početnici ili već iskusni pčelari, pružajući im sve potrebne informacije na jednom mestu.",
+    tech: ["Next.js 14", "Tailwind CSS", "shadCn"],
     status: "In progress",
   },
 ];
