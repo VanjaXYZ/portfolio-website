0ca62f3be73d17391ae546af02d9e0261355bb2b
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-27 15:35:31 +0100

    created

---
+++ src/app/_lib/projects-data.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Online Book Library",
+    type: "Fullstack",
+    short_description: "Create your own online book library!",
+    tech: ["Next.js 14", "Nest.js", "TailwindCSS"],
+    status: "Completed",
+  },
+  {
+    id: 2,
+    title: "My AI Diary",
+    type: "Fullstack",
+    short_description: "Write your daily diary entries with AI helper...",
+    tech: ["Next.js 14", "Nest.js", "TailwindCSS", "shadCn"],
+    status: "In progress",
+  },
+  {
+    id: 3,
+    title: "Football Stats",
+    type: "Frontend",
+    short_description: "Check the newest results in world of soccer...",
+    tech: ["Next.js 14", "TailwindCSS", "shadCn"],
+    status: "In progress",
+  },
+  {
+    id: 4,
+    title: "Beehive",
+    type: "Frontend",
+    short_description: "Learn all about beekeeping in one place..",
+    tech: ["Next.js 14", "TailwindCSS", "shadCn"],
+    status: "In progress",
+  },
+];
