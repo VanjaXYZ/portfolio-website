8a7a60cd64b087cb5a21b9f06f1fb4f5e002f4da
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-27 23:04:39 +0100

    refactor

--- src/app/(components)/About.tsx
+++ src/app/(components)/About.tsx
@@ -1,3 +1,4 @@
+import AboutMeMessage from "./AboutMeMessage";
 import { AvatarImageMobile } from "./AvatarImage";
 
 const About = () => {
@@ -8,16 +9,7 @@ const About = () => {
     >
       <AvatarImageMobile />
       <h2 className="text-2xl font-bold tracking-wider">About me</h2>
-      <article>
-        <p className="xl:text-xl text-sm tracking-wider lg:text-lg">
-          Hello! My name is <strong>Vanja Sukurma</strong>, a passionate
-          frontend developer with over 2 years of practical experience. I
-          specialize in React and Next.js environments, focusing on creating
-          intuitive and user-friendly web applications. In my work, I emphasize
-          efficiency, scalability, and aesthetics, and I enjoy the challenges
-          that come with the dynamic world of web technologies.
-        </p>
-      </article>
+      <AboutMeMessage />
     </section>
   );
 };
