7d8c376331778767f426847b9e4949bac33ce8ea
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-27 16:18:33 +0100

    add Link to heading

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="justify-between px-12 py-2 sticky top-0 bg-black/90 sm:flex hidden z-10">
-      <p className="font-bold uppercase">Vanja Šukurma</p>
+      <Link href={"/"} className="font-bold uppercase">
+        Vanja Šukurma
+      </Link>
       <HeaderLinks />
     </header>
   );
