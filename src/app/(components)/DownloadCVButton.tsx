1d03ede48e72a30b95defa8023f1cc606cb50233
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-28 00:17:59 +0100

    animation added

--- src/app/(components)/DownloadCVButton.tsx
+++ src/app/(components)/DownloadCVButton.tsx
@@ -1,12 +1,15 @@
+"use client";
+import { motion } from "framer-motion";
 const DownloadCVButton = () => {
   return (
-    <a
+    <motion.a
       href={"/vanja_sukurma_cv.pdf"}
       download={"vanja_sukurma_cv.pdf"}
-      className="bg-white/5 rounded-md p-4 uppercase tracking-widest hover:bg-white/10"
+      className="bg-white/5 rounded-md p-4 uppercase tracking-widest text-neutral-100 hover:bg-white/10 text-center"
+      whileHover={{ scale: 1.05, width: "235px" }}
     >
       Download CV
-    </a>
+    </motion.a>
   );
 };
 
