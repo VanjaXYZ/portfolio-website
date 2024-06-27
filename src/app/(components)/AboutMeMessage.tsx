d21440cbe60c61af388ccaac25e39da34e8edb4a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-27 23:04:33 +0100

    created

---
+++ src/app/(components)/AboutMeMessage.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import { splitString } from "../utils/splitString";
+import { motion, Variants } from "framer-motion";
+
+const AboutMeMessage = () => {
+  const charVariants = {
+    hidden: { opacity: 0 },
+    reveal: { opacity: 1 },
+  };
+
+  const introText = `Hello! I am Vanja Šukurma, a passionate frontend  developer with over 2 years of practical experience. I specialize in  React and Next.js environments, focusing on creating intuitive and  user-friendly web applications. In my work, I emphasize efficiency,  scalability, and aesthetics, and I enjoy the challenges that come with  the dynamic world of web technologies. 🚀`;
+
+  const splitIntroText = splitString(introText);
+  return (
+    <motion.p
+      className="xl:text-xl text-sm tracking-wider lg:text-lg"
+      initial="hidden"
+      whileInView="reveal"
+      transition={{ staggerChildren: 0.01 }}
+    >
+      {splitIntroText.map((char: string, index: number) => (
+        <motion.span
+          key={char + index}
+          transition={{ duration: 0.35 }}
+          variants={charVariants}
+        >
+          {char}
+        </motion.span>
+      ))}
+    </motion.p>
+  );
+};
+
+export default AboutMeMessage;
