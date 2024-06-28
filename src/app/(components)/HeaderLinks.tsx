7aae34153119285ddc007293f6c0641e88d2af67
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-28 09:40:27 +0100

    add animated bg on hover

--- src/app/(components)/HeaderLinks.tsx
+++ src/app/(components)/HeaderLinks.tsx
@@ -2,36 +2,40 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const HeaderLinks = () => {
   const pathname = usePathname();
+  const [hovered, setHovered] = useState("about");
+  const nav_links = ["about", "work", "contact"];
   return (
     <nav className="flex space-x-8">
-      <Link
-        href={"#about"}
-        className={clsx("font-bold tracking-widest", {
-          "text-white/10 line-through pointer-events-none": pathname !== "/",
-        })}
-      >
-        About
-      </Link>
-      <Link
-        href={"#work"}
-        className={clsx("font-bold tracking-widest", {
-          "text-white/10 line-through pointer-events-none": pathname !== "/",
-        })}
-      >
-        Projects
-      </Link>
-      <Link
-        href={"#contact"}
-        className={clsx("font-bold tracking-widest", {
-          "text-white/10 line-through pointer-events-none": pathname !== "/",
-        })}
-      >
-        Contact
-      </Link>
+      {nav_links.map((link: string, index: number) => (
+        <motion.button
+          onHoverStart={() => setHovered(link)}
+          key={`link-item-${index + 1}`}
+        >
+          <Link
+            href={`#${link}`}
+            className={clsx(
+              "relative font-bold tracking-widest px-5 py-1 uppercase",
+              {
+                "text-white/10 line-through pointer-events-none":
+                  pathname !== "/",
+              }
+            )}
+          >
+            {link}
+            {hovered === link && (
+              <motion.span
+                layoutId="nav-item"
+                className="absolute bg-gray-100 bg-opacity-10 inset-0 rounded-md"
+              ></motion.span>
+            )}
+          </Link>
+        </motion.button>
+      ))}
     </nav>
   );
 };
