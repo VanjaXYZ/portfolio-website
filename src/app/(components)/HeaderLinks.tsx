684f55565687d926bd4ba1a07362e73b7a30bf6a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-28 01:11:18 +0100

    disable links on condition

--- src/app/(components)/HeaderLinks.tsx
+++ src/app/(components)/HeaderLinks.tsx
@@ -1,16 +1,35 @@
+"use client";
+import clsx from "clsx";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const HeaderLinks = () => {
+  const pathname = usePathname();
   return (
     <nav className="flex space-x-8">
-      <Link href={"#about"} className="font-bold tracking-widest">
+      <Link
+        href={"#about"}
+        className={clsx("font-bold tracking-widest", {
+          "text-white/10 line-through pointer-events-none": pathname !== "/",
+        })}
+      >
         About
       </Link>
-      <Link href={"#work"} className="font-bold tracking-widest">
+      <Link
+        href={"#work"}
+        className={clsx("font-bold tracking-widest", {
+          "text-white/10 line-through pointer-events-none": pathname !== "/",
+        })}
+      >
         Projects
       </Link>
-      <Link href={"#contact"} className="font-bold tracking-widest">
+      <Link
+        href={"#contact"}
+        className={clsx("font-bold tracking-widest", {
+          "text-white/10 line-through pointer-events-none": pathname !== "/",
+        })}
+      >
         Contact
       </Link>
     </nav>
