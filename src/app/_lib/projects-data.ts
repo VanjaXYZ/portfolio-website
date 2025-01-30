e31bef1ff2116bca66c7b3ae18092d25134f8d7d
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2025-01-30 14:38:52 +0100

    add 2 new projects to /work section

--- src/app/_lib/projects-data.ts
+++ src/app/_lib/projects-data.ts
@@ -5,7 +5,7 @@ export const projects = [
     type: "Fullstack",
     short_description: "Create your own online book library",
     complete_description:
-      "Online Book Library je sveobuhvatna aplikacija koja omogućava korisnicima da kreiraju i upravljaju svojim digitalnim bibliotekama. Korisnici mogu dodavati knjige, uređivati informacije o knjigama, pratiti svoj napredak u čitanju i deliti preporuke sa prijateljima. Aplikacija koristi Next.js 14 za brzu i efikasnu izradu frontend dela, dok Nest.js obezbeđuje robustnu i skalabilnu backend arhitekturu. Tailwind CSS je korišćen za kreiranje responzivnog i atraktivnog korisničkog interfejsa. Ovaj projekat predstavlja kompletno rešenje za sve ljubitelje knjiga koji žele da organizuju i uživaju u svojoj kolekciji na mreži.",
+      "Online Book Library is a comprehensive application that allows users to create and manage their digital libraries. Users can add books, edit book information, track their reading progress, and share recommendations with friends. The application utilizes Next.js 14 for fast and efficient frontend development, while Nest.js provides a robust and scalable backend architecture. Tailwind CSS is used to create a responsive and attractive user interface. This project offers a complete solution for book lovers who want to organize and enjoy their collection online.",
     tech: ["Next.js 14", "Nest.js", "Tailwind CSS"],
     status: "Completed",
     link: "https://adders-library.vercel.app/",
@@ -16,9 +16,9 @@ export const projects = [
     type: "Fullstack",
     short_description: "Write your daily diary entries with AI helper",
     complete_description:
-      "My AI Diary je inovativna aplikacija koja koristi veštačku inteligenciju kako bi pomogla korisnicima da pišu i organizuju svoje dnevničke zapise. Ova aplikacija omogućava korisnicima da lako beleže svoje misli, osećanja i događaje iz svakodnevnog života, uz pomoć AI asistenta koji pruža sugestije, analizira emocije i predlaže poboljšanja. Sa Next.js 14, frontend je brz i responzivan, dok Nest.js osigurava snažnu backend podršku. shadCn i Tailwind CSS su korišćeni za moderni i privlačni dizajn korisničkog interfejsa. Ovaj projekat je idealan za sve koji žele da unaprede svoje pisanje i bolje razumeju sebe kroz interaktivne i inteligentne sugestije.",
+      "My AI Diary is an innovative application that leverages artificial intelligence to help users write and organize their diary entries. This app allows users to easily record their thoughts, feelings, and everyday events, with the assistance of an AI assistant that provides suggestions, analyzes emotions, and offers improvements. With Next.js 14, the frontend is fast and responsive, while Nest.js ensures robust backend support. shadCN and Tailwind CSS have been used to create a modern and attractive user interface. This project is perfect for anyone looking to enhance their writing and gain deeper self-awareness through interactive and intelligent suggestions.",
     tech: ["Next.js 14", "Nest.js", "shadCn", "Tailwind CSS"],
-    status: "In progress",
+    status: "",
     link: "https://my-diary-ai.vercel.app/",
   },
   {
@@ -27,9 +27,9 @@ export const projects = [
     type: "Frontend",
     short_description: "Check the newest results in world of soccer",
     complete_description:
-      "Football Stats je moderna aplikacija dizajnirana za sve ljubitelje fudbala koji žele da prate najnovije rezultate, statistike i vesti iz sveta fudbala. Aplikacija pruža korisnicima ažurirane informacije o utakmicama, timovima i igračima, omogućavajući im da ostanu informisani o svim važnim dešavanjima. Koristeći Next.js 14 za izradu frontend dela, aplikacija nudi brzo i efikasno korisničko iskustvo, dok Tailwind CSS i shadCn doprinose stvaranju modernog i privlačnog dizajna. Ovaj projekat je u toku i predstavlja izvrsno rešenje za sve koji žele da budu u toku sa svetskim fudbalskim događajima.",
+      "Football Stats is a modern application designed for football enthusiasts who want to keep track of the latest results, statistics, and news from the football world. The app provides users with up-to-date information on matches, teams, and players, allowing them to stay informed about all major events. Utilizing Next.js 14 for frontend development, the application delivers a fast and efficient user experience, while Tailwind CSS and shadCN contribute to a modern and visually appealing design. This ongoing project aims to be an excellent solution for anyone looking to stay up-to-date with global football events.",
     tech: ["Next.js 14", "Tailwind CSS", "shadCn"],
-    status: "In progress",
+    status: "",
   },
   {
     id: 4,
@@ -37,8 +37,31 @@ export const projects = [
     type: "Frontend",
     short_description: "Learn all about beekeeping in one place",
     complete_description:
-      "Beehive je sveobuhvatna aplikacija namenjena svima koji žele da nauče više o pčelarstvu. Ova aplikacija pruža korisnicima pristup bogatom sadržaju o pčelama, tehnikama pčelarstva, opremi, kao i savetima za početnike i iskusne pčelare. Sa Next.js 14, frontend aplikacije je brz i responzivan, dok Tailwind CSS i shadCn omogućavaju kreiranje atraktivnog i modernog dizajna. Beehive je savršeno mesto za sve koji žele da istraže svet pčelarstva, bilo da su početnici ili već iskusni pčelari, pružajući im sve potrebne informacije na jednom mestu.",
+      "Beehive is an application designed for anyone interested in learning more about beekeeping. The app offers users rich content on bees, beekeeping techniques, equipment, and valuable tips for both beginners and experienced beekeepers. Built with Next.js 14, the frontend is fast and responsive, while Tailwind CSS and shadCN contribute to an appealing and modern design. Beehive is the perfect hub for exploring the world of beekeeping, providing all the essential information in one place, whether you're just starting out or a seasoned beekeeper.",
     tech: ["Next.js 14", "Tailwind CSS", "shadCn"],
-    status: "In progress",
+    status: "",
+  },
+  {
+    id: 5,
+    title: "Rim Sped",
+    type: "Wordpress",
+    short_description: "Website for a transportation/logistics company",
+    complete_description:
+      "Rim Sped is a modern website representing a transport and logistics company. This website provides users with information about the company's services and contact details. By utilizing the WordPress platform, creating and maintaining the website becomes simple and efficient, allowing for quick and easy content updates. This project is ideal for companies looking to showcase their business online and attract new clients.",
+    tech: ["WordPress"],
+    status: "Completed",
+    link: "https://www.rimsped.com/",
+  },
+  {
+    id: 6,
+    title: "Meal Planning Application",
+    type: "Frontend",
+    short_description:
+      "This application allows users to meticulously plan meals through an interactive user interface.",
+    complete_description:
+      "Using dynamic tabs, users can input different food items, update quantities, and track the total nutritional values of each meal. Users can add food by entering the name, macronutrient type (carbohydrates, proteins, fats), and quantity in grams. The app automatically calculates calories, carbohydrates, proteins, and fats for each meal based on the entered items. Each meal is displayed in a separate tab. Users can easily add new meals and switch between them. Users can select a date to view specific meals for that day. All data is saved in LocalStorage to remain accessible for future visits. A summary section provides an overview of total calories and macronutrients for each meal in a clear format.",
+    tech: ["JavaScript, HTML, CSS"],
+    status: "Completed",
+    link: "https://www.tablicakalorija.com/",
   },
 ];
